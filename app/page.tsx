96037a124b12279b1a4350007a0910bf0787fb77
George <gakpovwovwo@gmail.com>
2023-05-10 10:57:02 +0100

    edit 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
